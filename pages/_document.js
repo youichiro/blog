b97e5b1873572250036278b6c658f354ad8c433d
youichiro <cinnamon416@gmail.com>
2022-06-12 12:52:05 +0900

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,8 @@ class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"></meta>
-          <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"></meta>
+          <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
+          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
