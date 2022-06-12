165c598aaaa674a6d2629cbd325cb648f853a655
youichiro <cinnamon416@gmail.com>
2022-06-12 23:30:14 +0900

    fix

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   image: '/static/images/avatar.jpg',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/my-twitter-card.png',
   email: 'cinnamon416@gmail.com',
   github: 'https://github.com/youichiro',
   twitter: 'https://twitter.com/youichiroz',
