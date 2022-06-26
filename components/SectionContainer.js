4c1e12df2799bd556d37a5d3cc928310a5ef1850
youichiro <cinnamon416@gmail.com>
2022-06-26 18:56:13 +0900

    fix

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,3 @@
 export default function SectionContainer({ children }) {
-  return <div className="mx-auto max-w-3xl sm:px-6 xl:max-w-5xl">{children}</div>
+  return <div className="mx-auto max-w-3xl xl:max-w-5xl">{children}</div>
 }
